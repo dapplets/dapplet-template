0941dabaae3d006adef542ad84da2c069df10373
Alexander Sakhaev <alsakhaev@gmail.com>
2020-02-03 16:33:26 +0300

    serving

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
 import image from '@rollup/plugin-image';
+import serve from 'rollup-plugin-serve';
 
 export default [{
     input: 'src/index.ts',
     output: [{
-        file: 'lib/index.min.js',
-        format: 'esm'
+        file: 'lib/index.js',
+        format: 'cjs',
+        exports: 'named'
     }],
-    plugins: [typescript(), image()]
+    plugins: [typescript(), image(), serve()]
 }];
