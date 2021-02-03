0601303c4cfd688588d72cf6e72d5bd8b5ee0306
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-04 01:17:46 +0300

    feat: prepare template for the example 1.

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,26 @@ export default class TwitterFeature {
   @Inject('twitter-adapter.dapplet-base.eth') public adapter: any;
   
   activate() {
+    // LP: 11. Use async method `Core.storage.get(serverUrl: string)` to get server url.
+    
+    // LP End
+    // LP: 12. Take a connection with server. Use `Core.connect<{ param }>({ url })`.
+
+    // LP End
     const { button } = this.adapter.exports;
     this.adapter.attachConfig({
       POST_SOUTH: [
         button({
           initial: 'DEFAULT',
           DEFAULT: {
-            label: 'Injected Button',
             img: EXAMPLE_IMG,
+            // LP: 1. Add label with counter for it.
+
+            // LP end
+            // LP: 2. Listen for the button click - output into console.
+            //     3: Make counter incrementing on button click.
             exec: () => alert('Hello, World!'),
+            // LP end
           },
         }),
       ],
