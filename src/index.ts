08b5bee0dcf3ddd6505fdcd6ab9ba37cf6b0ad8c
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-04 01:17:46 +0300

    feat: prepare template for the example 15

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,39 @@
 import { } from '@dapplets/dapplet-extension';
-import EXAMPLE_IMG from './icons/dapplet-icon.png';
+import EXAMPLE_IMG from './icons/ex15-19px.png';
+
+// LP: 2.1 Implement IDappState interface
+
+// LP end
 
 @Injectable
 export default class TwitterFeature {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
   @Inject('twitter-adapter.dapplet-base.eth') public adapter: any;
 
-  activate() {
+  async activate() {
+    // LP: 1. Get the URL from the config. Use Core.storage
+    
+    // LP end
+    // LP: 2.2 Create a connection with the server. Use Core.connect. Also create a common state
+    
+    // LP end
+
     const { button } = this.adapter.exports;
     this.adapter.attachConfig({
-      POST: () =>
-        button({
+      POST: (ctx: any) => {
+        // LP: 3. Pass observable value of server's state to common state
+        
+        // LP end
+        return button({
           initial: 'DEFAULT',
           DEFAULT: {
-            label: 'Injected Button',
             img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
+            // LP: 4. Pass observable counter to the label and in `exec` increase its value
+            
+            // LP end
           },
-        }),
+        });
+      },
     });
   }
 }
