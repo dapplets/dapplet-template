e7c0ada6bc3797c1f41b159960462af54952b602
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-04 01:49:21 +0300

    feat: solution for the example 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {} from '@dapplets/dapplet-extension'
 import COOL_BADGE_IMG from './icons/smile19.png'
-// import ANGRY_BADGE_IMG from './icons/angry-smile19.png'
+import ANGRY_BADGE_IMG from './icons/angry-smile19.png'
 
 @Injectable
 export default class TwitterFeature {
@@ -12,15 +12,20 @@ export default class TwitterFeature {
     this.adapter.attachConfig({
       POST: () =>
         button({
+          initial: 'DEFAULT',
           DEFAULT: {
             label: 'Fake',
             img: COOL_BADGE_IMG,
             // LP: 2. Add function toggling the button state
-
+            exec: (_, me) => (me.state = 'ANOTHER'),
             // LP end
           },
           // LP: 1. Add another state to the button
-
+          ANOTHER: {
+            label: 'FAKE!!!',
+            img: ANGRY_BADGE_IMG,
+            exec: (_, me) => (me.state = 'DEFAULT'),
+          },
           // LP end
         }),
     })
