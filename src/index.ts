f4286e72b76fdc58b7c30a820d934ce2e7de519f
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-04 01:17:46 +0300

    feat: solution for the example 1.

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,18 @@ export default class TwitterFeature {
           DEFAULT: {
             img: EXAMPLE_IMG,
             // LP: 1. Add label with counter for it.
-
+            label: 0,
             // LP end
             // LP: 2. Listen for the button click - output into console.
             //     3: Make counter incrementing on button click.
-            exec: () => Core.alert('Hello, World!'),
-            // LP end
+            exec: async (_, me) => {
+              console.log(ctx)
+              console.log(me)
+              me.label += 1
+              const message1 = await Core.storage.get('exampleString')
+              const message2 = await Core.storage.get('exampleHiddenString')
+              Core.alert(`I wrote: ${message1}. Then wrote: ${message2}.`)
+            },
           },
         }),
     })
