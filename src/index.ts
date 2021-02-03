6d399791821cbeb97b5b2748357ca539268b40e3
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-04 02:14:33 +0300

    feat: solution for the example 3.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {} from '@dapplets/dapplet-extension';
-import EXAMPLE_IMG from './icons/icon19.png';
+import STAMP_IMG from './icons/fakeStamp.png';
+import BADGE_IMG from './icons/angry-smile19.png';
 
 @Injectable
 export default class TwitterFeature {
@@ -8,7 +9,7 @@ export default class TwitterFeature {
   
   activate() {
     // LP: 1. Get the element 'picture' from adapter
-    const { button } = this.adapter.exports;
+    const { button, picture } = this.adapter.exports;
     // LP end
     // $ returns object "me". Use it to change state or params of the other element
     // Example 1: exec: (ctx) => $(ctx, 'another_el_id').state = 'SECOND'
@@ -19,16 +20,27 @@ export default class TwitterFeature {
           id: 'button',
           initial: 'DEFAULT',
           DEFAULT: {
-            label: 'Injected Button',
-            img: EXAMPLE_IMG,
+            label: 'FAKE',
+            img: BADGE_IMG,
             // LP: 2. Toggle the state “hidden/shown” of the picture on button click
-
+            exec: (ctx) => {
+              $(ctx, 'pic').hidden = !$(ctx, 'pic').hidden;
+            },
             // LP end
           },
         }),
       ],
-      // LP: 1. Add extra picture to POST_PICTURE and make it hidden by default
-
+      // LP: 1. Add extra picture to POST_PICTURE
+      POST_PICTURE: [
+        picture({
+          id: 'pic',
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: STAMP_IMG,
+            hidden: true,
+          },
+        }),
+      ],
       // LP end
     });
   }
