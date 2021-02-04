72d04b1f15e94208f86b52dc63261b2df75d53ed
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-04 18:22:45 +0300

    feat: solution for the example 4.

--- overlayWithReact/src/App.tsx
+++ overlayWithReact/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 // LP:  1. Import Bridge class from @dapplets/dapplet-overlay-bridge pachage:
+import Bridge from '@dapplets/dapplet-overlay-bridge'
 // LP end
 
 interface Props {}
@@ -11,6 +12,11 @@ interface State {
 }
 
 // LP:  2. Create `IDappletApi` interface and Bridge class instance typing with the inteface.
+interface IDappletApi {
+  increaseCounterAndToggleLabel: (isTick: boolean) => Promise<number>
+}
+
+const bridge = new Bridge<IDappletApi>()
 // LP end
 
 export default class App extends React.Component<Props, State> {
@@ -22,11 +28,16 @@ export default class App extends React.Component<Props, State> {
 
   componentDidMount() {
     // LP:  3. Add a listener to the 'data' event
+    bridge.on('data', ({ message, counter }: { message: string; counter: number }) =>
+      this.setState({ message, counter })
+    )
     // LP end
   }
 
   handleClick = async () => {
     // LP:  4. Add an event on the button click
+    const counter = await bridge.increaseCounterAndToggleLabel(this.state.isTick)
+    this.setState({ isTick: !this.state.isTick, counter })
     // LP end
   }
 
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import EXAMPLE_IMG from './icons/ex04.png'
 
 interface IDappletApi {
   // LP:  1. Implement the  IDappletApi interface, the same as in the React-based overlay.
+  increaseCounterAndToggleLabel: (isTick: boolean) => number
   // LP end
 }
 
@@ -17,10 +18,18 @@ export default class TwitterFeature {
   activate() {
     const { button } = this.adapter.exports
     // LP:  2. Create an overlay object using Core API: `Core.overlay({ name: string, title: string })`.
-
+    const overlay = Core.overlay({ name: 'example-04-overlay', title: 'Example 4' })
     // LP end
     // LP:  3. Create an obgect that implements the interface. Write increaseCounterAndToggleLabel function. Declare the API in the overlay.
-
+    const dappletApi: IDappletApi = {
+      increaseCounterAndToggleLabel: (isTick: boolean) => {
+        this.currentContext.counter =
+          this.currentContext.counter === undefined ? 1 : this.currentContext.counter + 1
+        this._currentProxy.label = `${isTick ? 'tick' : 'tock'} ${this.currentContext.counter}`
+        return this.currentContext.counter
+      },
+    }
+    overlay.declare(dappletApi)
     // LP end
     this.adapter.attachConfig({
       POST: (ctx) =>
@@ -31,8 +40,11 @@ export default class TwitterFeature {
             img: EXAMPLE_IMG,
             exec: async (_, proxy) => {
               // LP:  4. Save current context and proxy to the global variables to use them in `increaseCounterAndToggleLabel` function.
+              this.currentContext = ctx
+              this._currentProxy = proxy
               // LP end
               // LP:  5. By click open the overlay using `send()` method. Send 'Hello, World!' message and ctx.counter to the overlay using 'data' event.
+              overlay.send('data', { message: 'Hello, World!', counter: ctx.counter })
               // LP end
             },
           },
