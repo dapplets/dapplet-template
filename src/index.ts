cf7a5b70f9d3835ff5a398608d210b8407f72671
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-09 17:08:57 +0300

    feat: prepare template for the example 5.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
 import {} from '@dapplets/dapplet-extension'
-import EXAMPLE_IMG from './icons/dapplet-icon.png'
+import EXAMPLE_IMG from './icons/ex05.png'
 
 @Injectable
 export default class TwitterFeature {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
   @Inject('twitter-adapter.dapplet-base.eth') public adapter: any
 
-  activate() {
+  async activate() {
+    // LP: 1. Get the **wallet** object and common variables: `transferAmountEth`, `transferAmountNear` and `currentEthAddresses`
+
+    // LP end
+
     const { button } = this.adapter.exports
     this.adapter.attachConfig({
-      POST: () =>
-        button({
+      POST: () => {
+        return button({
           initial: 'DEFAULT',
           DEFAULT: {
-            label: 'Injected Button',
+            label: 'Connect',
             img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
+            // LP: 3. Connect the wallet
+
+            // LP end
           },
-        }),
+          // LP: 2. Add states ETH_CONNECTED, NEAR_CONNECTED, COMPLETED, FAILURE, REJECTED, PENDING and MINING
+
+          // LP end
+        })
+      },
     })
   }
 }
