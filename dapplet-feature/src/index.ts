2602471b8cf38739cc65a77bf8517f1b39cea9f2
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-16 16:24:09 +0300

    feat: solution for the example 8.

--- dapplet-feature/src/index.ts
+++ dapplet-feature/src/index.ts
@@ -4,7 +4,7 @@ import EXAMPLE_IMG from './icons/ex08.png'
 @Injectable
 export default class GoogleFeature {
   // LP: 1. Insert the correct adapter name
-
+  @Inject('example-google-adapter.dapplet-base.eth')
   public adapter
   // LP end
 
@@ -19,13 +19,24 @@ export default class GoogleFeature {
             tooltip: 'Show alert',
             // LP: 2. Log the POST context information to the console and show an alert
             //        with the title of the search result item on the button click.
-
+            exec: () => {
+              console.log('ctx:', ctx)
+              Core.alert('Title: ' + ctx.title)
+            },
             // LP end
           },
         }),
       // LP: 3. Add an avatar badge to the profile icon and log
       //        the PROFILE context information to the console.
-
+      PROFILE: (ctx) =>
+        avatarBadge({
+          DEFAULT: {
+            vertical: 'bottom',
+            horizontal: 'right',
+            img: EXAMPLE_IMG,
+            exec: () => console.log('ctx:', ctx),
+          },
+        }),
       // LP end
     })
   }
