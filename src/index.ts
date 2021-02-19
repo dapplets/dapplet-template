d2a7d6ef42258565d23af1b19f1dd414bd77074b
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-02-19 11:12:46 +0300

    fix: small style changes in src/index.js.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
-import { } from '@dapplets/dapplet-extension'
-//import { T_TwitterFeatureConfig, ITwitterAdapter } from '@dapplets/twitter-adapter'
-import EXAMPLE_IMG from './icons/icon19.png'
+import { } from '@dapplets/dapplet-extension';
+import EXAMPLE_IMG from './icons/icon19.png';
 
 @Injectable
 export default class TwitterFeature {
     constructor(
-        @Inject("twitter-adapter.dapplet-base.eth")
+        @Inject('twitter-adapter.dapplet-base.eth')
         public adapter: any //ITwitterAdapter;
     ) {
         const { button } = this.adapter.exports;
-
         this.adapter.attachConfig({
             POST_SOUTH: [
                 button({
-                    initial: "DEFAULT",
-                    "DEFAULT": {
-                        label: "Injected Button",
+                    initial: 'DEFAULT',
+                    DEFAULT: {
+                        label: 'Injected Button',
+                        img: EXAMPLE_IMG,
                         exec: (ctx, me) => alert('Hello, World!'),
-                        img: EXAMPLE_IMG
-                    }
-                })
-            ]
+                    },
+                }),
+            ],
         });
     }
 
