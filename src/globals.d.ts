e468e56bbacd68b2cb8be6261a93ae13436ee475
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-03-01 18:23:06 +0300

    feat: prepare template for the example 3.

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -2,3 +2,8 @@ declare module '*.png' {
   const content: string
   export default content
 }
+
+declare module '*.jpg' {
+  const content: string
+  export default content
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
 import {} from '@dapplets/dapplet-extension'
-import EXAMPLE_IMG from './icons/dapplet-icon.png'
+import EXAMPLE_IMG from './icons/ex03.png'
 
 @Injectable
 export default class TwitterFeature {
   @Inject('twitter-config.dapplet-base.eth')
   public adapter
 
-  activate() {
+  async activate() {
+    // LP: 1. Get the widget "avatarBadge"  from adapter
     const { button } = this.adapter.exports
-    this.adapter.attachConfig({
-      POST: () =>
+    // LP end
+    // $ returns object "me". Use it to change state or params of the other widget
+    // Example 1: exec: () => $(ctx, 'another_el_id').state = 'SECOND'
+    // Example 2: exec: () => $(ctx, 'another_el_id').label = 'Hello'
+    const { $ } = this.adapter.attachConfig({
+      POST: (ctx) => [
         button({
           DEFAULT: {
-            label: 'Injected Button',
+            label: 'GOLD',
             img: EXAMPLE_IMG,
-            exec: () => Core.alert('Hello, World!'),
+            // LP: 3. Toggle the state “hidden/shown” of the "avatarBadge" widget on button click
+
+            // LP end
           },
         }),
+        // LP: 2. Add extra "avatarBadge" widget and make it hidden by default
+
+        // LP end
+      ],
     })
   }
 }
