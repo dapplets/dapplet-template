20e57e5e24fc7a72773f5c25ab1b42a6a517b1ba
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-03-01 17:04:42 +0300

    fix: small change in src/index.ts to evoid prettier mistake.

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import EXAMPLE_IMG from './icons/icon19.png';
 @Injectable
 export default class TwitterFeature {
   constructor(
-    @Inject('twitter-adapter.dapplet-base.eth')
     // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
-    public adapter: any,
+    @Inject('twitter-adapter.dapplet-base.eth') public adapter: any,
   ) {
     const { button } = this.adapter.exports;
     this.adapter.attachConfig({
