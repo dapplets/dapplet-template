4f6535ef5ed5d807ee2fb71b44a1073f2f0f3782
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-03-04 12:21:33 +0300

    feat: solution for the example 9.

--- adapter/src/button.ts
+++ adapter/src/button.ts
@@ -18,7 +18,30 @@ export class Button {
   public mount(): void {
     if (!this.el) this._createElement();
     // LP: 2. implement the button HTML with "image" and "tooltip".
-
+    const { img, tooltip } = this.state;
+    const htmlString = `
+      <div style="
+        position:fixed;
+        width:60px;
+        height:60px;
+        bottom:40px;
+        left:40px;
+        background-color:#fff;
+        color:#FFF;
+        border-radius:50px;
+        border: 3px solid #d10019;
+        text-align:center;
+        box-shadow: 2px 2px 3px #999;
+        cursor: pointer;
+        display: block;
+        box-sizing: content-box;
+        z-index: 9999;
+      ">
+        <img style="margin-top:11px;" height="32" src="${img}">
+      </div>
+    `;
+    this.el.title = tooltip ?? '';
+    this.el.innerHTML = htmlString;
     // LP end
   }
 
--- dapplet-feature/src/index.ts
+++ dapplet-feature/src/index.ts
@@ -6,10 +6,10 @@ export default class ViewportFeature {
   constructor(
     // LP: 5. Add a valid adapter.
     // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
-    @Inject('') public adapter: any, // LP end
+    @Inject('exercise-viewport-adapter.dapplet-base.eth') public adapter: any, // LP end
   ) {
-    const { button } = this.adapter.exports;
-    this.adapter.attachConfig({
+    const { button, popup } = this.adapter.exports;
+    const { $ } = this.adapter.attachConfig({
       BODY: [
         button({
           initial: 'DEFAULT',
@@ -17,12 +17,20 @@ export default class ViewportFeature {
             label: 'Injected Button',
             img: EXAMPLE_IMG,
             // LP: 7. On button click show the popup
-
+            exec: (ctx) => ($(ctx, 'popup').closed = false),
             // LP end
           },
         }),
         // LP: 6. Add popup with text, image and some link from the page
-
+        popup({
+          id: 'popup',
+          initial: 'DEFAULT',
+          DEFAULT: {
+            text: 'The link opens this page in a new tab',
+            img: EXAMPLE_IMG,
+            init: (ctx, me) => (me.link = ctx.id),
+          },
+        }),
         // LP end
       ],
     });
