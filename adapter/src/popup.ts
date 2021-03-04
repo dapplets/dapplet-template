b64105f01a0a259d613c2932281a24304669c9ff
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-03-04 12:21:33 +0300

    feat: exercise for the example 9.

---
+++ adapter/src/popup.ts
@@ -0,0 +1,44 @@
+// import CLOSE_ICON from './close_cross.svg';
+
+export interface IPopupState {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  exec?: (ctx: any, me: IPopupState) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  init?: (ctx: any, me: IPopupState) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ctx?: any;
+  text?: string;
+  link?: string;
+  img?: string;
+  closed?: boolean;
+}
+
+export class Popup {
+  public el: HTMLElement;
+  public state: IPopupState;
+  public insPointName: string;
+
+  public mount(): void {
+    if (!this.el) this._createElement();
+    // LP: 3. Implement the popup HTML with "text", "link", "img" and "closed".
+
+    // LP end
+  }
+
+  public unmount(): void {
+    this.el && this.el.remove();
+  }
+
+  private _createElement() {
+    this.el = document.createElement('div');
+    this.state.closed = true;
+    this.el.style.position = 'static';
+    this.el.addEventListener('click', (e) => {
+      const target = e.target as HTMLElement;
+      if (!target.classList.contains('dapplet-widget-close-icon')) return;
+      this.state.closed = true;
+    });
+    this.mount();
+    this.state.init?.(this.state.ctx, this.state);
+  }
+}
--- dapplet-feature/src/index.ts
+++ dapplet-feature/src/index.ts
@@ -2,22 +2,28 @@ import {} from '@dapplets/dapplet-extension';
 import EXAMPLE_IMG from './icons/icon19.png';
 
 @Injectable
-export default class TwitterFeature {
+export default class ViewportFeature {
   constructor(
+    // LP: 5. Add a valid adapter.
     // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
-    @Inject('twitter-adapter.dapplet-base.eth') public adapter: any,
+    @Inject('') public adapter: any, // LP end
   ) {
     const { button } = this.adapter.exports;
     this.adapter.attachConfig({
-      POST_SOUTH: [
+      BODY: [
         button({
           initial: 'DEFAULT',
           DEFAULT: {
             label: 'Injected Button',
             img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
+            // LP: 7. On button click show the popup
+
+            // LP end
           },
         }),
+        // LP: 6. Add popup with text, image and some link from the page
+
+        // LP end
       ],
     });
   }
