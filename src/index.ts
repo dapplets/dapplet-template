240b922056613d692b6d4f5d327c00156bacfb91
Nikita Teremovskiy <ni-2@mail.ru>
2021-04-23 17:14:12 +0300

    feat: solution for the example 11.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,47 @@
 import {} from '@dapplets/dapplet-extension';
-import EXAMPLE_IMG from './icons/icon19.png';
+
+interface NftMetadata {
+  image: string;
+  text: string;
+}
 
 @Injectable
 export default class TwitterFeature {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
   @Inject('twitter-adapter.dapplet-base.eth') public adapter: any;
-  
-  activate() {
-    const { button } = this.adapter.exports;
+
+  private async _fetchNfts(url: string): Promise<NftMetadata[]> {
+    const res = await fetch(url);
+    return res.json();
+  }
+
+  async activate() {
+    const { label, button } = this.adapter.exports;
+    const url = await Core.storage.get('dataUrl');
     this.adapter.attachConfig({
-      POST_SOUTH: [
-        button({
-          initial: 'DEFAULT',
-          DEFAULT: {
-            label: 'Injected Button',
-            img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
-          },
-        }),
-      ],
+      POST_USERNAME_LABEL: async (ctx) => {
+        const nfts = await this._fetchNfts(url);
+        return nfts.map((nft) =>
+          label({
+            DEFAULT: {
+              img: nft.image,
+              basic: true,
+              tooltip: nft.text + ' of ' + ctx.authorFullname,
+            },
+          }),
+        );
+      },
+      PROFILE_BUTTON_GROUP: async (ctx) => {
+        const nfts = await this._fetchNfts(url);
+        return nfts.map((nft) =>
+          button({
+            DEFAULT: {
+              img: nft.image,
+              tooltip: nft.text + ' of ' + ctx.authorFullname,
+            },
+          }),
+        );
+      },
     });
   }
 }
