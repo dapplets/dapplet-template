3c0c761be3d774f2555fc2a57b9e0d4adff660f6
Nikita Teremovskiy <ni-2@mail.ru>
2021-05-21 17:32:45 +0300

    feat: testing context-oriented instance points and regular ones.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {} from '@dapplets/dapplet-extension';
 import EXAMPLE_IMG from './icons/icon19.png';
+import BIG_IMG from './icons/icon64.png';
 
 @Injectable
 export default class TwitterFeature {
@@ -7,18 +8,158 @@ export default class TwitterFeature {
   @Inject('twitter-adapter.dapplet-base.eth') public adapter: any;
   
   activate() {
-    const { button } = this.adapter.exports;
-    this.adapter.attachConfig({
-      POST_SOUTH: [
-        button({
-          initial: 'DEFAULT',
-          DEFAULT: {
-            label: 'Injected Button',
-            img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
-          },
-        }),
-      ],
-    });
+    const { badge, label, button, picture } = this.adapter.exports;
+    const config = {
+      // TIMELINE: async (timelineCtx, me) => {
+        // ...userData = await Server.get();
+        TWEET: (ctx, me) => [
+          // ...tweetData = await Server.get(userData);
+          badge({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              vertical: 'bottom',
+              horizontal: 'right',
+              img: EXAMPLE_IMG,
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+          button({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              label: 'new',
+              img: EXAMPLE_IMG,
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+          label({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              basic: true,
+              img: EXAMPLE_IMG,
+              text: 'new label',
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+          picture({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              img: BIG_IMG,
+            },
+          }),
+        ],
+        PROFILE: async (ctx, me) => [
+          badge({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              vertical: 'bottom',
+              horizontal: 'right',
+              img: BIG_IMG,
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+          button({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              img: BIG_IMG,
+              label: 'new#1',
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+          button({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              img: BIG_IMG,
+              label: 'new#2',
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+        ],
+
+        // old design support
+        PROFILE_AVATAR_BADGE: async (ctx, me) => [
+          badge({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              vertical: 'bottom',
+              horizontal: 'left',
+              img: BIG_IMG,
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+        ],
+        POST_SOUTH: async (ctx, me) => [
+          button({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              label: 'old#1',
+              img: EXAMPLE_IMG,
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+          button({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              label: 'old#2',
+              img: EXAMPLE_IMG,
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+        ],
+        PROFILE_BUTTON_GROUP: async (ctx, me) => [
+          button({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              label: 'old',
+              img: EXAMPLE_IMG,
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+        ],
+        POST_USERNAME_LABEL: async (ctx, me) => 
+          label({
+            initial: 'DEFAULT',
+            DEFAULT: {
+              basic: true,
+              img: EXAMPLE_IMG,
+              text: 'old label',
+              exec: () => {
+                console.log('ctx = ', ctx);
+                console.log('me = ', me);
+              },
+            },
+          }),
+      //}
+    };
+    this.adapter.attachConfig(config);
   }
 }
