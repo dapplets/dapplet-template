8c2cb201b76f985a2d82f7deb3e5a312fc10ac96
Nikita Teremovskiy <ni-2@mail.ru>
2021-06-04 18:41:26 +0300

    feat: example for nestes-ctxs v.1. Add ctx.parent.

--- src/index.ts
+++ src/index.ts
@@ -13,184 +13,199 @@ export default class TwitterFeature {
   activate() {
     const { avatar, avatarBadge, usernameBadge, label, button, picture, caption } = this.adapter.exports;
     const config = {
-        POST: (ctx) => [
-          // STARTER
-          [
-            {
-              label: 'Add tweet to the Ethereum registry',
-              exec: (ctx) => console.log('ctx1 = ', ctx),
+      /* This is also the correct way to use it:
+      REPOST: (repostCtx) => [
+        button({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            label: 'repost',
+            img: EXAMPLE_IMG,
+            exec: () => {
+              console.log('repostCtx = ', repostCtx)
+              console.log('parent ctx = ', repostCtx.parent)
             },
-            {
-              label: 'Add tweet to the NEAR registry',
-              exec: (ctx) => console.log('ctx2 = ', ctx),
-            },
-            {
-              label: 'Add tweet to the Swarm',
-              exec: (ctx) => console.log('ctx3 = ', ctx),
-            },
-          ],
-          {
-            RETWEET: (retweetCtx) => [
-              button({
-                initial: 'DEFAULT',
-                DEFAULT: {
-                  label: 'new',
-                  img: EXAMPLE_IMG,
-                  exec: () => {
-                    console.log('tweetCtx = ', ctx);
-                    console.log('retweetCtx = ', retweetCtx)
-                  },
-                },
-              }),
-            ],
           },
-          avatar({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BOY,
-            },
-          }),
-          usernameBadge({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: { DARK, LIGHT },
-              exec: () => {
-                console.log('ctx = ', ctx);
+        }),
+      ],
+      */
+      POST: (ctx) => [
+        {
+          REPOST: (repostCtx) => [
+            button({
+              initial: 'DEFAULT',
+              DEFAULT: {
+                label: 'repost',
+                img: EXAMPLE_IMG,
+                exec: () => {
+                  console.log('repostCtx = ', repostCtx)
+                  console.log('parent ctx = ', repostCtx.parent)
+                },
               },
-            },
-          }),
-          avatarBadge({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              vertical: 'bottom',
-              horizontal: 'right',
-              img: { DARK, LIGHT },
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
-            },
-          }),
-          button({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              label: 'new',
-              img: EXAMPLE_IMG,
-              exec: () => {
-                $(ctx, 'pic').hidden = !$(ctx, 'pic').hidden;
-              },
-            },
-          }),
-          label({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              basic: true,
-              img: EXAMPLE_IMG,
-              text: 'new label',
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
-            },
-          }),
-          picture({
-            id: 'pic',
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BIG_IMG,
-            },
-          }),
-          caption({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: EXAMPLE_IMG,
-              text: 'new caption',
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
-            },
-          }),
+            }),
+          ],
+        },
+        // STARTER
+        [
+          {
+            label: 'Add tweet to the Ethereum registry',
+            exec: (ctx) => console.log('ctx1 = ', ctx),
+          },
+          {
+            label: 'Add tweet to the NEAR registry',
+            exec: (ctx) => console.log('ctx2 = ', ctx),
+          },
+          {
+            label: 'Add tweet to the Swarm',
+            exec: (ctx) => console.log('ctx3 = ', ctx),
+          },
         ],
-        PROFILE: async (ctx) => [
-          avatar({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BOY,
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
+        avatar({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BOY,
+          },
+        }),
+        usernameBadge({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: { DARK, LIGHT },
+            exec: () => {
+              console.log('ctx = ', ctx);
             },
-          }),
-          usernameBadge({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BIG_IMG,
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
+          },
+        }),
+        avatarBadge({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            vertical: 'bottom',
+            horizontal: 'right',
+            img: { DARK, LIGHT },
+            exec: () => {
+              console.log('ctx = ', ctx);
             },
-          }),
-          avatarBadge({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              vertical: 'bottom',
-              horizontal: 'right',
-              img: BIG_IMG,
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
+          },
+        }),
+        button({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            label: 'button',
+            img: EXAMPLE_IMG,
+            exec: () => {
+              $(ctx, 'pic').hidden = !$(ctx, 'pic').hidden;
             },
-          }),
-          button({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BIG_IMG,
-              label: 'new#1',
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
+          },
+        }),
+        label({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            basic: true,
+            img: EXAMPLE_IMG,
+            text: 'label',
+            exec: () => {
+              console.log('ctx = ', ctx);
             },
-          }),
-          button({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BIG_IMG,
-              label: 'new#2',
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
+          },
+        }),
+        picture({
+          id: 'pic',
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BIG_IMG,
+          },
+        }),
+        caption({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: EXAMPLE_IMG,
+            text: 'caption',
+            exec: () => {
+              console.log('ctx = ', ctx);
             },
-          }),
-        ],
-        SUSPENDED: (ctx) => [
-          avatar({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BOY,
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
+          },
+        }),
+      ],
+      PROFILE: async (ctx) => [
+        avatar({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BOY,
+            exec: () => {
+              console.log('ctx = ', ctx);
             },
-          }),
-          usernameBadge({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BIG_IMG,
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
+          },
+        }),
+        usernameBadge({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BIG_IMG,
+            exec: () => {
+              console.log('ctx = ', ctx);
             },
-          }),
-        ],
-        HEADING: (ctx) => [
-          usernameBadge({
-            initial: 'DEFAULT',
-            DEFAULT: {
-              img: BIG_IMG,
-              exec: () => {
-                console.log('ctx = ', ctx);
-              },
+          },
+        }),
+        avatarBadge({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            vertical: 'bottom',
+            horizontal: 'right',
+            img: BIG_IMG,
+            exec: () => {
+              console.log('ctx = ', ctx);
             },
-          }),
-        ],
+          },
+        }),
+        button({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BIG_IMG,
+            label: 'button#1',
+            exec: () => {
+              console.log('ctx = ', ctx);
+            },
+          },
+        }),
+        button({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BIG_IMG,
+            label: 'button#2',
+            exec: () => {
+              console.log('ctx = ', ctx);
+            },
+          },
+        }),
+      ],
+      HEADING: (ctx) => [
+        usernameBadge({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BIG_IMG,
+            exec: () => {
+              console.log('ctx = ', ctx);
+            },
+          },
+        }),
+      ],
+      SUSPENDED: (ctx) => [
+        avatar({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BOY,
+            exec: () => {
+              console.log('ctx = ', ctx);
+            },
+          },
+        }),
+        usernameBadge({
+          initial: 'DEFAULT',
+          DEFAULT: {
+            img: BIG_IMG,
+            exec: () => {
+              console.log('ctx = ', ctx);
+            },
+          },
+        }),
+      ],
     };
     const { $ } = this.adapter.attachConfig(config);
   }
