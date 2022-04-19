dd6acb86781c42a7ff99037cd0812d26de93f531
Andrei Aleksandrov <krasnijlajm@gmail.com>
2022-04-19 20:21:11 +0200

    ex 06 done

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import { } from '@dapplets/dapplet-extension';
+import {} from '@dapplets/dapplet-extension';
 import EXAMPLE_IMG from './icons/dapplet-icon.png';
 
 @Injectable
 export default class TwitterFeature {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
-  @Inject('twitter-adapter.dapplet-base.eth') public adapter: any;
+  @Inject('common-adapter.dapplet-base.eth') public adapter: any;
 
   activate() {
     const { button } = this.adapter.exports;
     this.adapter.attachConfig({
-      POST: () =>
+      BODY: () =>
         button({
-          initial: 'DEFAULT',
           DEFAULT: {
-            label: 'Injected Button',
+            tooltip: 'Injected Button',
             img: EXAMPLE_IMG,
             exec: () => alert('Hello, World!'),
           },
