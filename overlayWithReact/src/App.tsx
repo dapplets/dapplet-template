8de5c29faf17e933c64f319fd3cae77c2c7beea7
Andrei Aleksandrov <krasnijlajm@gmail.com>
2022-04-19 15:52:40 +0200

    #4 done

--- overlayWithReact/src/App.tsx
+++ overlayWithReact/src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 // LP:  1. Import Bridge class from @dapplets/dapplet-overlay-bridge pachage:
-
+import Bridge from '@dapplets/dapplet-overlay-bridge';
 // LP end
 
 interface Props {}
 
 interface State {
-  message: string | null
-  counter: number
-  isTick: boolean
+  message: string | null;
+  counter: number;
+  isTick: boolean;
 }
 
 // LP:  2. Create `IDappletApi` interface and Bridge class instance typing with the inteface.
+interface IDappletApi {
+  increaseCounterAndToggleLabel: (isTick: boolean) => Promise<number>;
+}
 
+const bridge = new Bridge<IDappletApi>();
 // LP end
 
 export default class App extends React.Component<Props, State> {
@@ -24,13 +28,16 @@ export default class App extends React.Component<Props, State> {
 
   componentDidMount() {
     // LP:  3. Add a listener to the 'data' event
-    
+    bridge.on('data', ({ message, counter }: { message: string; counter: number }) =>
+      this.setState({ message, counter }),
+    );
     // LP end
   }
 
-  handleClick = async() => {
+  handleClick = async () => {
     // LP:  4. Add an event on the button click
-    
+    const counter = await bridge.increaseCounterAndToggleLabel(this.state.isTick);
+    this.setState({ isTick: !this.state.isTick, counter });
     // LP end
   };
 
@@ -40,7 +47,9 @@ export default class App extends React.Component<Props, State> {
         <h1>Overlay with ReactJS</h1>
         <p>Message from the dapplet: {this.state.message}</p>
         <p>Counter: {this.state.counter ?? 0}</p>
-        <button className="ch-state-btn" onClick={this.handleClick}>Counter +1</button>
+        <button className="ch-state-btn" onClick={this.handleClick}>
+          Counter +1
+        </button>
       </>
     );
   }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import EXAMPLE_IMG from './icons/ex04.png';
 
 interface IDappletApi {
   // LP:  1. Implement the  IDappletApi interface, the same as in the React-based overlay.
-
+  increaseCounterAndToggleLabel: (isTick: boolean) => Promise<number>,
   // LP end
 }
 
@@ -23,16 +23,26 @@ export default class TwitterFeature {
             img: EXAMPLE_IMG,
             exec: async (_, me) => {
               // LP:  2. Implement the overlay opening on the button click. To get the current overlay use `Core.overlay({ name: string, title: string })`.
-              
+              const overlay = Core.overlay({ name: 'example-04-overlay', title: 'Example 4' });
               // LP end
 
               // LP:  3. Create an obgect that implements the interface. Write increaseCounterAndToggleLabel function. Declare the API in the overlay.
-              
+              const dappletApi: IDappletApi = {
+                increaseCounterAndToggleLabel: (isTick: boolean) => {
+                  ctx.counter = ctx.counter === undefined ? 1 : ctx.counter + 1;
+                  me.label = `${isTick ? 'tick' : 'tock'} ${ctx.counter}`;
+                  return ctx.counter;
+                }
+              };
+
+              overlay.declare(dappletApi);
               // LP end
 
               // LP:  4. Send 'Hello, World!' message and ctx.counter to the overlay using 'data' event.
-              
+              overlay.send('data', { message: 'Hello, World!', counter: ctx.counter });
               // LP end
+
+              overlay.onClose(() => console.log('The overlay closed!'));
             },
           },
         }),
