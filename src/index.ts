cdad6c70349ec02ef50a6dea6f8b65dc7b100e87
Andrei Aleksandrov <krasnijlajm@gmail.com>
2022-04-19 21:15:10 +0200

    ex 7 partly done

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import EXAMPLE_IMG from './icons/dapplet-icon.png';
 @Injectable
 export default class TwitterFeature {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
-  @Inject('twitter-adapter.dapplet-base.eth') public adapter: any;
+  @Inject('identity-adapter.dapplet-base.eth') public adapter: any;
 
   activate() {
     const { button } = this.adapter.exports;
