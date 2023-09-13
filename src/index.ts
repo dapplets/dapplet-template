4ed203af65ebd4f93fd12355e7dae8cf13931a60
Lisofffa <lisofffa@gmail.com>
2023-09-13 23:49:53 +0300

    fix: fixed master example

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,44 @@ import EXAMPLE_IMG from './icons/dapplet-icon.png'
 @Injectable
 export default class TwitterFeature {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
-  @Inject('twitter-adapter.dapplet-base.eth') public adapter: any
+  @Inject('twitter-config.dapplet-base.eth')
+  public adapter
+  private _config: any
+  private _adapterDescription: {
+    contextsNames: string[]
+    widgets?: any
+  }
+  private _$: any
+  private _globalContext = {}
+  async activate(): Promise<void> {
+    console.log('this.adapter', this.adapter)
+    if (this.adapter._attachedConfig) {
+      const contextsNames = Object.keys(this.adapter._attachedConfig)
+      console.log(contextsNames)
 
-  activate() {
+      this._adapterDescription = { contextsNames, widgets: {} }
+    }
+
     const { button } = this.adapter.exports
-    this.adapter.attachConfig({
-      POST: () =>
-        button({
-          initial: 'DEFAULT',
-          DEFAULT: {
-            label: 'Injected Button',
-            img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
-          },
-        }),
-    })
+
+    this._config = {
+      GLOBAL: (global) => {
+        Object.assign(this._globalContext, global)
+      },
+      POST: (ctx) => {
+        return [
+          button({
+            DEFAULT: {
+              id: 'button',
+              label: 'Injected Button',
+              img: EXAMPLE_IMG,
+              exec: async () => await Core.alert('Hello Word!'),
+            },
+          }),
+        ]
+      },
+    }
+    const { $ } = this.adapter.attachConfig(this._config)
+    this._$ = $
   }
 }
