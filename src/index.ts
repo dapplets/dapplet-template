086c655306b9a637f76edb4ab937763bb1cd8799
Lisofffa(lisofffa.near) <lisofffa@gmail.com>
2023-09-20 14:21:34 +0300

    feat: move to mv3 adapter
    * fix: fixed master example
    
    * fix: fixed main example
    
    * fix: small fixes
    
    * fix: remove id
    
    ---------
    
    Co-authored-by: Nikita Teremovskiy <ni-2@mail.ru>

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,18 @@ import EXAMPLE_IMG from './icons/dapplet-icon.png'
 
 @Injectable
 export default class TwitterFeature {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
-  @Inject('twitter-adapter.dapplet-base.eth') public adapter: any
+  @Inject('twitter-config.dapplet-base.eth')
+  public adapter
 
   activate() {
     const { button } = this.adapter.exports
     this.adapter.attachConfig({
       POST: () =>
         button({
-          initial: 'DEFAULT',
           DEFAULT: {
             label: 'Injected Button',
             img: EXAMPLE_IMG,
-            exec: () => alert('Hello, World!'),
+            exec: () => Core.alert('Hello, World!'),
           },
         }),
     })
